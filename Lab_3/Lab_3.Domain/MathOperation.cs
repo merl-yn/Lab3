394d124328c23db0771ec354df4c90a183d502eb
merl-yn <84568391+merl-yn@users.noreply.github.com>
2021-05-21 13:05:07 +0300

    Add files via upload

---
+++ Lab_3/Lab_3.Domain/MathOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab_3.Domain
+{
+    public class MathOperationEventArgs : EventArgs
+    {
+        public double Value1 { get; }
+        public double Value2 { get; }
+
+        public MathOperationEventArgs(double value1, double value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+    }
+    
+    public interface IMathOperation
+    {
+        event EventHandler<MathOperationEventArgs> OnDivision;
+        double Sum(double a, double b);
+        double Multiple(double a, double b);
+        double Divide(double a, double b);
+        double Subtract(double a, double b);
+    }
+    
+    public class MathOperation : IMathOperation
+    {
+        public event EventHandler<MathOperationEventArgs> OnDivision;
+
+        public double Sum(double a, double b) => a + b;
+
+        public double Multiple(double a, double b) => a * b;
+
+        public double Subtract(double a, double b) => a - b;
+
+        public double Divide(double a, double b)
+        {
+            OnDivision?.Invoke(this, new MathOperationEventArgs(a, b));
+            return a / b;
+        }
+    }
+}
---
+++ Lab_3/Lab_3.Host/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Lab_3.Domain;
+
+namespace Lab_3.Host
+{
+    class Program
+    {
+        private static string _stringExpression = "10 / 5 = 2";
+        
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+            
+            var mathOperation = new MathOperation();
+            mathOperation.OnDivision += OnDivision;
+            
+            mathOperation.OnDivision += delegate(object? sender, MathOperationEventArgs e)
+            {
+                var words = _stringExpression.Split(" /=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                var hitsCount = words.Count(str =>
+                    str == Convert.ToString(e.Value1) || str == Convert.ToString(e.Value2));
+            
+                Console.WriteLine($"String contains {hitsCount} symbols from math expression");
+            };
+
+            mathOperation.OnDivision += (sender, e) =>
+            {
+                var words = _stringExpression.Split(" /=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                var hitsCount = words.Count(str =>
+                    str == Convert.ToString(e.Value1) || str == Convert.ToString(e.Value2));
+            
+                Console.WriteLine($"String contains {hitsCount} symbols from math expression");
+            };
+
+            Console.WriteLine($"{a} + {b} = {mathOperation.Sum(a, b)}");
+            Console.WriteLine($"{a} - {b} = {mathOperation.Subtract(a, b)}");
+            Console.WriteLine($"{a} * {b} = {mathOperation.Multiple(a, b)}");
+            Console.WriteLine($"{a} / {b} = {mathOperation.Divide(a, b)}");
+        }
+
+        private static void OnDivision(object? sender, MathOperationEventArgs e)
+        {
+            var words = _stringExpression.Split(" /=".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var hitsCount = words.Count(str =>
+                str == Convert.ToString(e.Value1) || str == Convert.ToString(e.Value2));
+            
+            Console.WriteLine($"String contains {hitsCount} symbols from math expression");
+        }
+    }
+}
